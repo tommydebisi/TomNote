663ab567ba48f9e4106b08fb8581b1669cc12882
tommydebisi <tommydebisi@gmail.com>
2023-02-23 10:46:10 +0100

    :ssedling: removed extra param

--- views/__init__.py
+++ views/__init__.py
@@ -5,8 +5,6 @@ from flask import Blueprint
 app_notes = Blueprint(
     "app_notes",
     __name__,
-    template_folder="../templates",
-    static_folder="../static",
 )
 
 from views.notes import *
